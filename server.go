4a658325fd2e0f0505a20c2e9101617ce311a857
Artem Gusev <gusev2014russia@mail.ru>
2021-10-15 08:51:24 +0300

    add GET and POST handler for the server

--- server.go
+++ server.go
@@ -20,8 +20,27 @@ func helloHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello!\n")
 }
 
+func formHandler(writer http.ResponseWriter, request *http.Request) {
+	if err := request.ParseForm(); err != nil {
+		fmt.Fprintf(writer, "ParseForm() error %v", err)
+		return
+	}
+
+	fmt.Fprintf(writer, "Post request successful\n")
+
+	name := request.FormValue("name")
+	address := request.FormValue("address")
+
+	fmt.Fprintf(writer, "name = %s\n", name)
+	fmt.Fprintf(writer, "address = %s\n", address)
+}
+
 func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("Server is running on port 8000. http://localhost:8000/\n")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
